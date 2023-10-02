1f69f570af51cedc4a4af9ff819ebf209dfe4651
Tometa75 <tometa@mac.com>
2023-10-02 20:34:16 +0200

    psalm

---
+++ salmi/50.php
@@ -0,0 +1,81 @@
+<?php
+$this->closure['load']=function() {
+
+    $this->titolo='Salmo 50 Pietà di me, o Signore';
+
+    //lodi venerdì
+    $this->antifona[0]=array(
+        "Lavami,",
+        "o Dio, da ogni peccato; crea in me un cuore puro."
+    );
+
+    $this->antifona[1]=$this->antifona[0];
+
+    ///////////////////////////////////////////////////////////////
+
+    $this->testoBase=array(
+        'all'=>array(
+            array(
+                array('','*',"Pietà di me, o Dio, secondo la tua misericordia:"),
+                array('','2',"nel tuo grande amore cancella il mio pecdcato."),
+                array('','*',"Lavami da tutte le mie colpe,"),
+                array('','2',"mondami del mio peccato.")
+            ),
+            array(
+                array('','*',"Riconosco la mia colpa,"),
+                array('','2',"il mio peccato mi sta sempre dinanzi."),
+                array('','*',"Contro di te, contro te solo ho peccato,"),
+                array('','2',"quello che è male ai tuoi occhi, io l'ho fatto;"),
+                array('','*',"percià sei giusto quando parli,"),
+                array('','2',"retto nel tuo giudizio.")
+            ),
+            array(
+                array('','*',"Ecco, nella colpa sono stato generato,"),
+                array('','2',"nel peccatomi ha concepito mia madre."),
+                array('','*',"Ma tu vuoi la sincerità del cuore"),
+                array('','2',"e nell'intimo m'insegni la sapienza.")
+            ),
+            array(
+                array('','*',"Purificami con issòpo e sarò mondato;"),
+                array('','2',"lavami e sarò più bianco della neve."),
+                array('','*',"Fammi sentire gioia e letizia,"),
+                array('','2',"esulteranno le ossa che hai spezzato.")
+            ),
+            array(
+                array('','*',"Distogli lo sguardo dai miei peccati,"),
+                array('','2',"cancella tutte le mie colpe."),
+                array('','*',"Crea in me, o Dio, un cuore puro,"),
+                array('','2',"rinnova in me uno spirito saldo.")
+            ),
+            array(
+                array('','*',"Non respingermi dalla tua presenza"),
+                array('','2',"e non privarmi del tuo santo spirito."),
+                array('','*',"Rendimi la gioia di essere salvato,"),
+                array('','2',"sostieni in me un animo generoso.")
+            ),
+            array(
+                array('','*',"Insegnerò agli erranti le tue vie"),
+                array('','2',"e i peccatori a te ritorneranno."),
+                array('','*',"Liberami dal sangue, Dio, Dio mia salvezza,"),
+                array('','2',"la mia lingua esalterà la tua giustizia.")
+            ),
+            array(
+                array('','*',"Signore, apri le mie labbra"),
+                array('','2',"e la mia bocca proclami la tua lode;"),
+                array('','*',"poiché non gradisci iil scarificio"),
+                array('','2',"e, se offro olocausti, non li accetti."),
+                array('','*',"Uno spirito contrito è sacrificio a Dio,"),
+                array('','2',"un cuore affranto e umiliato, tu o Dio, non disprezzi.")
+            ),
+            array(
+                array('','*',"Nel tuo amore fa grazia a Sion,"),
+                array('','2',"rialza le mura di Gerusalemme."),
+                array('','+',"Allora gradirai i sacrifici prescritti,"),
+                array('','*',"l'olocausto e l'intera oblazione,"),
+                array('','2',"allora immoleranno vittime sopra il tuo altare.")
+            )
+        )
+    );
+
+}
+?>
---
+++ salmi/95.php
@@ -0,0 +1,72 @@
+<?php
+$this->closure['load']=function() {
+
+    $this->titolo="Salmo 95 Dio, re e giudice dell'universo";
+
+    //lodi venerdì
+    if ($this->actual['tempo']=='P') {
+        $this->antifona[0]=array(
+            "Alleluia,",
+            "alleluia, alleluia."
+        );
+    }
+    else {
+        $this->antifona[0]=array(
+            "Cantate a Dio,",
+            "benedite il suo nome! Dite tra i popoli: Il Signore regna."
+        );
+    }
+
+    $this->antifona[1]=array(
+        "Cantate a Dio,",
+        "benedite il suo nome! Dite tra i popoli: Il Signore regna."
+    );
+
+    ///////////////////////////////////////////////////////////////
+
+    $this->testoBase=array(
+        'all'=>array(
+            array(
+                array('','*',"Cantate al Singore un canto nuovo,"),
+                array('','2',"cantate al Signore da tutta la terra."),
+                array('','*',"Cantate al Signore, benedite il suo nome,"),
+                array('','2',"annunziate di giorno in giorno la sua salvezza."),
+                array('','*',"In mezzo ai popoli narrate la sua gloria,"),
+                array('','2',"a tutte le nazioni dite i suoi prodigi.")
+            ),
+            array(
+                array('','*',"Grande è il Signore e degno di ogni lode,"),
+                array('','2',"terribile sopra tutti gli dei."),
+                array('','*',"Tutti gli dei delle nazioni sono un nulla,"),
+                array('','2',"ma il Signore ha fatto i cieli."),
+                array('','*',"Maestà e bellezza sono davanti a lui,"),
+                array('','2',"potenza e splendore nel suo santuario.")
+            ),
+            array(
+                array('','+',"Date al Signore, o famiglie dei popoli,"),
+                array('','*',"date al Signore gloria e potenza,"),
+                array('','2',"date al Signore la gloria del suo nome."),
+                array('','*',"Portate offerte ed entrate nei suoi atri,"),
+                array('','2',"prostratevi al Signore in sacri ornamenti.")
+            ),
+            array(
+                array('','*',"Tremi davanti alui tutta la terra."),
+                array('','2',"Dite tra i popoli: « Il Signore regna! »."),
+                array('','*',"Sorregge il miondo, perché non vacilli;"),
+                array('','2',"giudica le nazioni con rettitudine.")
+            ),
+            array(
+                array('','+',"Gioiscano i cieli, esulti la terra,"),
+                array('','*',"frema il mare e quanto racchiude;"),
+                array('','2',"esultino i campi e quanto contengono,"),
+                array('','+',"si rallegrino gli alberi della foresta"),
+                array('','*',"davanti al Signore che viene,"),
+                array('','2',"perché viene a giudicare la terra."),
+                array('','*',"Giudicherà il mondo con giustizia"),
+                array('','2',"e con verità tutte le genti.")
+            )
+        )
+    );
+
+}
+?>
