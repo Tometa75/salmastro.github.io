18d3ba478a42fc50dffcccb75e99ecf3b44d26b5
Tometa75 <tometa@mac.com>
2024-02-06 00:10:21 +0100

    editing

--- classi/salmo.php
+++ classi/salmo.php
@@ -98,7 +98,7 @@ class Salmo {
 
     function TPAntifona() {
         //la funzione dà per scontato che l'antifona iniziale e finale siano uguali
-        $this->antifona[0]=$this->antifona[0].' Alleluia.';
+        $this->antifona[0][1]=$this->antifona[0][1].' Alleluia.';
         $this->antifona[1]=$this->antifona[0]; 
     }
 
