930d1e0814b4a3b9335de6623ae71b0cbfa2de4b
Tometa75 <137708329+Tometa75@users.noreply.github.com>
2024-02-20 22:28:31 +0100

    editing

--- litcal.php
+++ litcal.php
@@ -42,20 +42,30 @@ foreach ($res as $k=>$sett) {
                     elseif ($map['tempo']['codice']=='A') echo 'background-color:#c500ff22';
                 echo '">';
 
-                    echo '<div style="position:relative;display:inline-block;vertical-align:top;width:15%;cursor:pointer;" onclick="window._calnav_salmastro.setToday(\''.$d['tag'].'\')">';
-                        echo '<div style="font-weight:bold;">'.mainFunc::gab_todata($d['tag']).'</div>';
-                        echo '<div style="font-size:0.9em;">'.$map['tempo']['nome'].'</div>';
+                    echo '<div style="position:relative;display:inline-block;vertical-align:top;width:17%;cursor:pointer;padding:4px;box-sizing:border-box;';
+						//if ($map['weekDay']==0) echo 'background-color:#dddddd55;';
+					echo '" onclick="window._calnav_salmastro.setToday(\''.$d['tag'].'\')">';
+						echo '<div style="position:relative;padding:3px;box-sizing:border-box;min-height:48px;';
+							if ($map['weekDay']==0) echo 'border:2px solid black;';
+						echo '">';
+                        	echo '<div style="font-weight:bold;">'.mainFunc::gab_todata($d['tag']).'</div>';
+                        	echo '<div style="font-size:0.9em;">'.($map['rocho']==0?$map['tempo']['nome']:'').'</div>';
+						echo '</div>';
                     echo '</div>';
 
-                    echo '<div style="position:relative;display:inline-block;vertical-align:top;width:20%;" >';
+                    echo '<div style="position:relative;display:inline-block;vertical-align:top;width:20%;';
+						//if ($map['weekDay']==0) echo 'background-color:#55555555;';
+					echo '" >';
                         if ($map['settimana']!="") echo '<div>Settimana: '.$map['settimana'].'</div>';
-                        echo '<div>'.mainFunc::gab_weektotag($map['weekDay']).'</div>';
+                        echo '<div style="';
+							if ($map['weekDay']==0) echo 'font-weight:bold;';
+						echo '">'.mainFunc::gab_weektotag($map['weekDay']).'</div>';
                     echo '</div>';
 
                     //{"today":"20240101","tempo":{"codice":"N","nome":"Tempo di Natale","colore":"white","fine":"battesimo"},"anno":"B","settimana":"","quarto":"","weekDay":"1","pari":true,"festa":[],"evento":{"MSS":{"titolo":"Maria SS. Madre di Dio","tipo":"S","ppvv":true}},"rocho":false,"ASC":"20240512","errore":false}
                     //{"today":"20240102","tempo":{"codice":"N","nome":"Tempo di Natale","colore":"white","fine":"battesimo"},"anno":"B","settimana":"","quarto":"","weekDay":"2","pari":true,"festa":{"0102a":{"titolo":"Santi Basiglio Magno e Gregorio Nazianzeno (Vescovi e Dottori della Chiesa)","tipo":"M","comune":"pastori","mix":1}},"evento":{"F0102":{"titolo":"Ferie prima dell'Epifania","tipo":"F"}},"rocho":false,"ASC":"20240512","errore":false}
 
-                    echo '<div style="position:relative;display:inline-block;vertical-align:top;width:65%;" >';
+                    echo '<div style="position:relative;display:inline-block;vertical-align:top;width:63%;" >';
 
                         foreach ($map['evento'] as $evCode=>$e) {
                             echo '<div style="font-weight:bold;color:red;">'.$e['titolo'].'</div>';
